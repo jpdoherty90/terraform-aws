dd767b7cefd5d6111729d6b38c246c8f7f924eb7
jpdoherty90 <jpdoherty90@gmail.com>
2021-11-26 10:55:33 -0500

    Added vpc config to main

--- main.tf
+++ main.tf
@@ -1,15 +1,61 @@
 
+variable "region" {
+	type = string
+	default = "us-east-1"
+}
+
+variable "vpc_cidr_range" {
+	type = string
+	default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+	type = list(string)
+	default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "database_subnets" {
+	type = list(string)
+	default = ["10.0.8.0/24", "10.0.9.0/24"]
+}
+
 provider "aws" {
-
-
 	version = "~> 2.0"
-
-	alias = "networking"
-
 	region = var.region
+}
 
-	access_key = var.access_key
-	seret_key = var.secret_key
+data "aws_availability_zones" "azs" {}
 
+module "vpc" {
+	source = "terraform-aws-modules/vpc/aws"
+	version = "2.33.0"
 
+	name = "dev-vpc"
+	cidr = var.vpc_cidr_range
+
+	azs = slice(data.aws_availability_zones.azs.name, 0, 2)
+	public_subnets = var.public_subnets
+
+	database_subnets = var.database_subnets
+	database_subnet_group_tags = {
+		subnet_type = "database"
+	}
+
+	tags = {
+		Environment = "dev"
+		Team = "infra"
+	}
+
+}
+
+output "vpc_id" {
+	value = module.vpc.vpc_id
+}
+
+output "db_subnet_group" {
+	value = module.vpc.database_subnet_group
+}
+
+output "public_subnets" {
+	value = module.vpc.public_subnets
 }
